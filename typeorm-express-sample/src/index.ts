e8f5ccc4b487b89644c47664c6d64dbc894dbac0
xiongzhiyao <xzy1994@hotmail.com>
2020-11-10 22:01:16 -0800

    first commit

---
+++ typeorm-express-sample/src/index.ts
@@ -0,0 +1,58 @@
+import "reflect-metadata";
+import { createConnection, ConnectionOptions } from "typeorm";
+import * as express from "express";
+import * as bodyParser from "body-parser";
+import {Request, Response} from "express";
+import {Routes} from "./routes";
+import {User} from "./entity/User";
+
+createConnection(
+  <ConnectionOptions>{
+    type: "postgres",
+    extra: {  ssl: process.env.DATABASE_URL ? true : false, },
+    // Change the next line to use the Heroku postgresql from other environment like localhost, remenber that heroku changes this data periodically for security reasons
+    url: process.env.DATABASE_URL || "postgres://test:test@localhost:5432/test", 
+    entities: [ "src/entity/**/*.ts" ],
+    subscribers: [],
+    synchronize: true,
+  }
+).then(async connection => {
+
+    // create express app
+    const app = express();
+    app.use(bodyParser.json());
+
+    // register express routes from defined application routes
+    Routes.forEach(route => {
+        (app as any)[route.method](route.route, (req: Request, res: Response, next: Function) => {
+            const result = (new (route.controller as any))[route.action](req, res, next);
+            if (result instanceof Promise) {
+                result.then(result => result !== null && result !== undefined ? res.send(result) : undefined);
+
+            } else if (result !== null && result !== undefined) {
+                res.json(result);
+            }
+        });
+    });
+
+    // setup express app here
+    // ...
+
+    // start express server
+    app.listen(3000);
+
+    //insert new users for test
+    await connection.manager.save(connection.manager.create(User, {
+        firstName: "Timber",
+        lastName: "Saw",
+        age: 27
+    }));
+    await connection.manager.save(connection.manager.create(User, {
+        firstName: "Phantom",
+        lastName: "Assassin",
+        age: 24
+    }));
+
+    console.log("Express server has started on port 3000. Open http://localhost:3000/users to see results");
+
+}).catch(error => console.log(error));
